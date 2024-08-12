1b8985f7006ca42d99d0ef59afd161eb42530084
lunashp <hijnshp@gmail.com>
2024-08-12 23:52:11 +0900

    fix: 회원가입 시 중복확인 및 css 수정중

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -44,7 +44,7 @@ const Input = styled("input")({
 const Button = styled("button")({
   borderRadius: "5px",
   width: "120px",
-  height: "36px",
+  height: "36x",
   color: "#c0c0c0",
   cursor: "pointer",
 });
@@ -63,93 +63,188 @@ const SignupButton = styled("button")({
   boxSizing: "border-box",
 });
 
-const ErrorMessage = styled("p")({
-  position: "absolute",
-  top: "42px",
-  left: "10px",
-  fontSize: "16px",
+// const ErrorMessage = styled("p")({
+//   position: "absolute",
+//   top: "42px",
+//   left: "10px",
+//   fontSize: "16px",
+//   color: "rgb(216, 1, 1)",
+// });
+
+const ErrorMessage = styled("li")({
+  // position: "",
+  // borderTop: "20px",
+  // borderBottom: "10px",
   color: "rgb(216, 1, 1)",
 });
 
 const SignupForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    passwordConfirm: "",
+  });
+
+  const router = useRouter();
+
+  // const checkEmailDuplicate = async () => {
+  //   if (!formData.email) {
+  //     setErrors((prev) => ({
+  //       ...prev,
+  //       email: "값을 입력해주세요.",
+  //     }));
+  //     return; // 이름이 비어 있으면 함수 종료
+  //   }
+
+  //   const response = await fetch("/api/auth/check-email", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ email: formData.email }),
+  //   });
+  //   const data = await response.json();
+  //   if (data.isDuplicate) {
+  //     setErrors((prev) => ({
+  //       ...prev,
+  //       email: "이 이메일은 이미 사용 중입니다.",
+  //     }));
+  //     setFormData((prev) => ({ ...prev, email: "" }));
+  //   } else {
+  //     setErrors((prev) => ({ ...prev, email: "" }));
+  //     alert("이 이메일을 사용할 수 있습니다.");
+  //   }
+  // };
 
   const checkEmailDuplicate = async () => {
+    // 이름이 비어 있는지 확인
+    if (!formData.email) {
+      setErrors((prev) => ({
+        ...prev,
+        email: "값을 입력해주세요.",
+      }));
+      return; // 이름이 비어 있으면 함수 종료
+    }
     const response = await fetch("/api/auth/check-email", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email }),
+      body: JSON.stringify({ email: formData.email }),
     });
     const data = await response.json();
     if (data.isDuplicate) {
-      alert("이 이메일은 이미 사용 중입니다.");
-      setEmail("");
-    }
-    // if (data.setEmail === "") {
-    //   alert("이메일을 입력해주세요");
-    // }
-    else {
+      setErrors((prev) => ({
+        ...prev,
+        email: "이 이메일은 이미 사용 중입니다.",
+      }));
+      setFormData((prev) => ({ ...prev, email: "" }));
+    } else {
+      setErrors((prev) => ({ ...prev, email: "" }));
       alert("이 이메일을 사용할 수 있습니다.");
     }
   };
 
   const checkNameDuplicate = async () => {
+    // 이름이 비어 있는지 확인
+    if (!formData.name) {
+      setErrors((prev) => ({
+        ...prev,
+        name: "값을 입력해주세요.",
+      }));
+      return; // 이름이 비어 있으면 함수 종료
+    }
+
     const response = await fetch("/api/auth/check-name", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name: name }),
+      body: JSON.stringify({ name: formData.name }),
     });
     const data = await response.json();
     if (data.isDuplicate) {
-      alert("이 닉네임은 이미 사용 중입니다.");
-      setName("");
+      setErrors((prev) => ({
+        ...prev,
+        name: "이 닉네임은 이미 사용 중입니다.",
+      }));
+      setFormData((prev) => ({ ...prev, name: "" }));
     } else {
+      setErrors((prev) => ({ ...prev, name: "" }));
       alert("이 닉네임을 사용할 수 있습니다.");
     }
   };
-  const router = useRouter();
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
   const handleSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
 
     // 비밀번호와 비밀번호 확인이 일치하는지 검사
-    if (password !== passwordConfirm) {
-      alert("비밀번호가 일치하지 않습니다.");
+    if (formData.password !== formData.passwordConfirm) {
+      setErrors((prev) => ({
+        ...prev,
+        passwordConfirm: "비밀번호가 일치하지 않습니다.",
+      }));
       return;
+    } else {
+      setErrors((prev) => ({ ...prev, password: "" }));
     }
 
     const response = await fetch("/api/auth/signup", {
       method: "POST",
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify(formData),
       headers: {
         "Content-Type": "application/json",
       },
     });
 
-    // const data = await response.json();
+    // // 비밀번호와 비밀번호 확인이 일치하는지 검사
+    // if (password !== passwordConfirm) {
+    //   alert("비밀번호가 일치하지 않습니다.");
+    //   return;
+    // }
+
+    // const response = await fetch("/api/auth/signup", {
+    //   method: "POST",
+    //   body: JSON.stringify({ name, email, password }),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // });
+
     const data = await response.status;
-    // console.log(data);
+
+    console.log("data", data);
 
     if (data === 201) {
       alert("사용자 생성됨!");
       router.push("/api/auth/signin");
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        email: "서버에서 오류가 발생했습니다.",
+      }));
     }
   };
 
-  // const signUpButton = (event: { preventDefault: () => void }) => {
-  //   console.log("event", event.preventDefault());
-  // };
-
   return (
     <Form onSubmit={handleSubmit}>
-      <InputList>
+      {/* <InputList>
         <ListItem>
           <InputDiv>
             <Input
@@ -161,9 +256,28 @@ const SignupForm = () => {
               onChange={(e) => setName(e.target.value)}
               style={{ marginRight: "8px" }}
             />
+
             <ErrorMessage>
               이미 사용 중인 닉네임입니다. 다른 닉네임을 입력해 주세요.
             </ErrorMessage>
+          </InputDiv>
+          <Button type="button" onClick={checkNameDuplicate}>
+            중복검사
+          </Button>
+        </ListItem> */}
+
+      <InputList>
+        <ListItem>
+          <InputDiv>
+            <Input
+              type="text"
+              name="name"
+              required
+              value={formData.name}
+              placeholder="닉네임 입력"
+              onChange={handleChange}
+            />
+            {errors.name && <ErrorMessage>{errors.name}</ErrorMessage>}
           </InputDiv>
           <Button type="button" onClick={checkNameDuplicate}>
             중복검사
@@ -172,17 +286,26 @@ const SignupForm = () => {
 
         <ListItem>
           <InputDiv>
-            <Input
+            {/* <Input
               type="text"
               id="email"
               required
-              value={email}
+              value={formData.email}
               placeholder="이메일 입력"
-              onChange={(e) => setEmail(e.target.value)}
+              // onChange={(e) => setEmail(e.target.value)}
+              onChange={handleChange}
             />
-            <ErrorMessage>
-              이미 등록된 이메일입니다. 다른 닉네임을 입력해 주세요.
-            </ErrorMessage>
+            {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
+          </InputDiv> */}
+            <Input
+              type="text"
+              name="email"
+              required
+              value={formData.email}
+              placeholder="이메일 입력"
+              onChange={handleChange}
+            />
+            {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
           </InputDiv>
           <Button type="button" onClick={checkEmailDuplicate}>
             중복검사
@@ -194,15 +317,16 @@ const SignupForm = () => {
               type="password"
               id="password"
               required
-              value={password}
+              value={formData.password}
               placeholder="비밀번호 입력"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleChange}
+              // onChange={(e) => setPassword(e.target.value)}
             />
           </InputDiv>
         </ListItem>
         <ListItem>
           <InputDiv>
-            <Input
+            {/* <Input
               type="password"
               id="passwordConfirm"
               value={passwordConfirm}
@@ -213,6 +337,18 @@ const SignupForm = () => {
             <ErrorMessage>
               비밀번호가 일치하지 않습니다. 다시 입력해주세요.
             </ErrorMessage>
+          </InputDiv> */}
+            <Input
+              type="passwordConfirm"
+              name="passwordConfirm"
+              required
+              value={formData.passwordConfirm}
+              placeholder="비밀번호 확인"
+              onChange={handleChange}
+            />
+            {errors.passwordConfirm && (
+              <ErrorMessage>{errors.passwordConfirm}</ErrorMessage>
+            )}
           </InputDiv>
         </ListItem>
       </InputList>
